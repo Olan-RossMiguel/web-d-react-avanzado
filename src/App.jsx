0389d51cb13ea599bc1895e3f33265c73f072233
Miguel <jesrous12@gmail.com>
2025-07-15 20:10:15 -0600

    creacion de custom hook useOllama

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import { yupResolver } from '@hookform/resolvers/yup'
 import './index.css'
 import * as yup from 'yup'
 import axios from 'axios'
-import { useState } from 'react'
+import { useReducer, useState } from 'react'
 
 const schema = yup.object({
   userInput: yup
@@ -12,6 +12,8 @@ const schema = yup.object({
     .required('El mensaje es obligatorio')
 })
 
+
+
 export const App = () => {
   const { register, handleSubmit, formState: { errors } } = useForm({
     resolver: yupResolver(schema)
@@ -19,22 +21,12 @@ export const App = () => {
   // Guarda la respuesta de llama2
   const [response, setResponse] = useState('')
   const [loading, setLoading] = useState(false)
+ 
 
   const handlePregunta = async (data) => {
     console.log(data)
     setLoading(true)
-    try {
-      const res = await axios.post('http://localhost:11434/api/generate', {
-        model: 'llama2',
-        prompt: data.userInput,
-        stream: false
-      })
-      setResponse(res.data.response)
-    } catch (error) {
-      console.error('error: ', error)
-    } finally {
-      setLoading(false)
-    }
+   
   }
 
   return (
@@ -51,8 +43,16 @@ export const App = () => {
         >Preguntar
         </button>
       </form>
-      <div>
+      {/* <div>
         <p>{loading ? 'Generando respuesta 🚀' : response}</p>
+      </div> */}
+      <div>
+        {state.messages.map((msg, index) => (
+          <p key={index}>
+            <strong>{msg.from === 'user' ? 'Tú' : 'Bot'}:</strong>
+            {msg.text}
+          </p>
+        ))}
       </div>
     </>
   )
---
+++ src/hooks/useOllama.js
@@ -0,0 +1,37 @@
+import axios from 'axios'
+import React from 'react'
+
+const initialState = {
+  messages: []
+}
+
+const chatReducer = (state, action) => {
+  switch (action.type) {
+    case 'ADD_MESSAGE':
+      console.log('agregando mensaje...')
+      console.log(state)
+      return { ...state, messages: [...state.messages, action.payload] }
+    default:
+      return state
+  }
+}
+
+export const useOllama = () => {
+  const [state, dispatch] = useReducer(chatReducer, initialState)
+  const sendMessage = async (userPrompt) => {
+    try {
+      const res = await axios.post('http://localhost:11434/api/generate', {
+        model: 'deepseek-r1:1.5b',
+        prompt: userPrompt,
+        stream: false
+      })
+      // Dispatch para guardar el mensaje del usuario
+      dispatch({ type: 'ADD_MESSAGE', payload: { from: 'user', text: userPrompt } })
+      dispatch({ type: 'ADD_MESSAGE', payload: { from: 'bot', text: res.data.response } })
+    } catch (error) {
+      console.error('error: ', error)
+    }
+  }
+
+  return { sendMessage }
+}
